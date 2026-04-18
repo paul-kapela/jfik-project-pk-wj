6f0c9ebdba108710e009eb82c76b448c55ce60e0
Wiktoria Jeleniewicz <jeleniewiczwiktoria@gmail.com>
2026-04-18 12:56:34 +0200

    Add arithmetic operations to LLVM generator and actions

--- LLVMActions.java
+++ LLVMActions.java
@@ -1,5 +1,21 @@
 import java.util.HashSet;
 
+enum VarType {
+    INT,
+    REAL,
+    REALD
+}
+
+class Value {
+    VarType type;
+    String value;
+
+    public Value(VarType type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+}
+
 public class LLVMActions extends ProjektBaseListener {
     HashSet<String> variables = new HashSet<String>();
     
